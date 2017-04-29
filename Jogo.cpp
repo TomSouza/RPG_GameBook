f126cc77e9375fd08a2c29b0e12b1ef76ec7ecb8
Thomas de Azevedo de Souza <Thomas de Azevedo de Souza>
2017-04-29 19:25:52 -0300

    Arquivos base do jogo
    Cria arquivos basicos para o funcionamento do jogo.

---
+++ Jogo.cpp
@@ -0,0 +1,29 @@
+#include "Jogo.h"
+
+Jogo::Jogo()
+{
+}
+
+Jogo::~Jogo()
+{
+}
+
+void Jogo::inicializar()
+{
+	uniInicializar(800, 600, false, "Tales of Rhagni");
+}
+
+void Jogo::finalizar()
+{
+	uniFinalizar();
+}
+
+void Jogo::executar()
+{
+	while(!gTeclado.soltou[TECLA_ESC] && !gEventos.sair)
+	{
+		uniIniciarFrame();
+
+		uniTerminarFrame();
+	}
+}
