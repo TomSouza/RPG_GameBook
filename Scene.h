21a7b123e0dbe4d91eb524c5c0d3c0d9a8a1a88c
Thomas de Azevedo de Souza <thomas.aes.31@hotmail.com>
2017-05-04 00:10:02 -0300

    Estrutura de cenas
    Modifica clase base de cenas adicionando atributo de inicialização.
    Modifica metodo update para ter retorno de Scenes e ser publico.

--- Scene.h
+++ Scene.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "libUnicornio.h"
 
+enum Scenes {
+    KEEP, INTRO, MAIN_MENU
+};
+
 class Scene
 {
 public:
@@ -9,11 +13,13 @@ public:
 
     virtual void start() = 0;
     virtual void finish() = 0;
+    virtual Scenes update() = 0;
 
+    bool started = false;
 protected:
-    virtual void update() = 0;
     virtual void draw() = 0;
 
     Sprite background;
+    Scenes sceneChange;
 };
 
