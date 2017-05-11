a7a727d5367c8ef34cb6b7afe1dab91f4e93040f
Thomas de Azevedo de Souza <thomas.aes.31@hotmail.com>
2017-05-11 00:50:36 -0300

    Enum cenas
    Adiciona valores ao enum de cena para diferenciar load de new game.

--- Scene.h
+++ Scene.h
@@ -3,7 +3,12 @@
 #include "AppModel.h"
 
 enum Scenes {
-    KEEP, INTRO, MAIN_MENU, PAUSE_MENU
+    KEEP,
+    INTRO,
+    MAIN_MENU,
+    NEW_GAME,
+    LOAD_GAME,
+    PAUSE_MENU
 };
 
 class Scene
