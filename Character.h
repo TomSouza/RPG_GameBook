cca7ad4b440af2dd4efb7908358268fbdb484264
Thomas de Azevedo de Souza <thomas.aes.31@hotmail.com>
2017-05-16 07:33:23 -0300

    Documenta os metodos da classe

--- Character.h
+++ Character.h
@@ -21,37 +21,124 @@ public:
     Character();
     ~Character();
 
+    /// <summary>
+    /// Calcula o dano que o personagem causara
+    /// </summary>
     virtual int attack() = 0;
+
+    /// <summary>
+    /// Defende de um golpe
+    /// </summary>
+    /// <param name="damage"> Quantia de dano recebido </param>
     virtual void defend(int damage) = 0;
 
+    /// <summary>
+    /// Define o nome do personagem
+    /// </summary>
+    /// <param name="value"> Nome </param>
     void setName(string value);
+
+    /// <summary>
+    /// Busca nome do personagem
+    /// </summary>
     string getName();
 
-    void setStrength(int);
+    /// <summary>
+    /// Define a força do personagem
+    /// </summary>
+    /// <param name="value"> Força </param>
+    void setStrength(int value);
+
+    /// <summary>
+    /// Busca força do personagem
+    /// </summary>
     int getStrength();
 
-    void setAbility(int);
+    /// <summary>
+    /// Define a habilidade do personagem
+    /// </summary>
+    /// <param name="value"> Habilidade </param>
+    void setAbility(int value);
+
+    /// <summary>
+    /// Busca habilidade do personagem
+    /// </summary>
     int getAbility();
 
-    void setEndurance(int);
+    /// <summary>
+    /// Define a resistencia do personagem
+    /// </summary>
+    /// <param name="value"> Resistencia </param>
+    void setEndurance(int value);
+
+    /// <summary>
+    /// Busca resistencia do personagem
+    /// </summary>
     int getEndurance();
 
-    void setArmor(int);
+    /// <summary>
+    /// Define o nivel de armadura do personagem
+    /// </summary>
+    /// <param name="value"> Armadura </param>
+    void setArmor(int value);
+
+    /// <summary>
+    /// Busca pontos de armadura do personagem
+    /// </summary>
     int getArmor();
 
-    void setFirePower(int);
+    /// <summary>
+    /// Define o poder de fogo do personagem
+    /// </summary>
+    /// <param name="value"> Poder de Fogo </param>
+    void setFirePower(int value);
+
+    /// <summary>
+    /// Busca poder de fogo do personagem
+    /// </summary>
     int getFirePower();
 
-    void setExperience(int);
+    /// <summary>
+    /// Atribui experiencia ao personagem
+    /// </summary>
+    /// <param name="value"> Experiencia </param>
+    void setExperience(int value);
+
+    /// <summary>
+    /// Busca experiencia do personagem
+    /// </summary>
     int getExperience();
 
-    void setHp(int);
+    /// <summary>
+    /// Define a saude do personagem
+    /// </summary>
+    /// <param name="value"> Saude </param>
+    void setHp(int value);
+
+    /// <summary>
+    /// Busca saude do personagem
+    /// </summary>
     int getHp();
 
-    void setMp(int);
+    /// <summary>
+    /// Define os pontos de mana do personagem
+    /// </summary>
+    /// <param name="value"> Mana </param>
+    void setMp(int value);
+
+    /// <summary>
+    /// Busca pontos de mana do personagem
+    /// </summary>
     int getMp();
 
+    /// <summary>
+    /// Busca poder de ataque do personagem
+    /// </summary>
     int getAP();
+
+    /// <summary>
+    /// Busca poder de defesa do personagem
+    /// </summary>
     int getDP();
 };
 
